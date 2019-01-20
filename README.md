5d6af31525130e4f6abf2b38c6b465bff09b5029
Ize Imai <izeimai@gmail.com>
2019-01-20 12:50:36 -0500

    Update README with code quotes

--- README.md
+++ README.md
@@ -2,9 +2,39 @@
 "Language Interpretation and Recognition Interface" is a command line Node app
 
 *What does this app do?*
+User can input request information for concerts, songs and movies through command-line terminal 
+
+```
+node liri.js *action* *value*
+```
+
+
+1) Search Bands In Town with `concert-this`
+
+```
+node liri.js concert-this *value*
+```
+
+2) Search Spotify with `spotify-this-song`
+
+```
+node liri.js spotify-this-song *value*
+```
+
+3) Search OMDB with `movie-this`
+
+```
+node liri.js movie-this *value*
+```
+
+3) Searching contents of text file with `do-what-it-says`
+
+```
+node liri.js do-what-it-says *<filename.extension>*
+```
 
 
 *How is this app useful?*
-This familiarized the npm package manager through using four different packages. Axios npm was used to capture requests and responses to APIs. I learned how to hide API keys by utilizing "dotenv" nmp package and ".gitignore" file that designates which files to not upload to GitHub. I can use this information to go back to the apps created in the first half of the program and hide their API keys.
+This familiarized the npm package manager through using multiple package modules. Axios npm was used to capture requests and responses to APIs. The dotenv package was used to set a process.env object. Reading text files used the fs package. I learned how to hide API keys by utilizing "dotenv" nmp package and ".gitignore" file that designates which files to not upload to GitHub. I can use this information to go back to the apps created in the first half of the program and hide their API keys.
 
 *Future directions*
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 // Load required packages and files
 require("dotenv").config();
 var keys = require("./keys.js");
+var fs = require("fs");
 
 //Keys can be retrieved with this:
 //var spotify = new Spotify(keys.spotify);
@@ -10,22 +11,72 @@ var action = process.argv[2];
 var value = process.argv[3];
 
 // switch-case statement for the entered command to determine which function runs
-var entertainment = function(action) {
-    switch(action){
-        case "concert-this":
+switch (action) {
+    case "concert-this":
         concertThis();
         break;
 
-        case "spotify-this-song":
+    case "spotify-this-song":
         spotifyThisSong();
         break;
 
-        case "movie-this":
+    case "movie-this":
         movieThis();
         break;
 
-        case "do-what-it-says":
+    case "do-what-it-says":
         doWhatItSays();
         break;
+}
+
+// Searches the Bands in Town Artist Events API to return concert info
+function concertThis() {
+    var queryURL = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp"
+    console.log(queryURL);
+
+    // Name of the venue
+    // Venue location
+    // Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+}
+
+//
+function spotifyThisSong() {
+    console.log("SpotifyThis");
+
+    // Default to "The Sign" by Ace of Base if no song is provided
+    if (value === undefined) {
+        console.log("No Song provided");
     }
+
+    // Artist(s)
+    // The song's name
+    // A preview link of the song from Spotify
+    // The album that the song is from
+
+
+}
+
+function movieThis() {
+    console.log("MovieThis");
+
+    // Default to "Mr. Nobody" if no movie is provided
+    if (value === undefined) {
+        console.log("No movie provided");
+        value = "Mr.Nobody"
+    } 
+
+    // Title of the movie.
+    // Year the movie came out.
+    // IMDB Rating of the movie.
+    // Rotten Tomatoes Rating of the movie.
+    // Country where the movie was produced.
+    // Language of the movie.
+    // Plot of the movie.
+    // Actors in the movie.
+ 
+}
+
+function doWhatItSays() {
+    console.log("dowhatitsays");
 }
