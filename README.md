59f8a9102e47c319329f7be09633ca4ca8ce5566
Ize Imai <izeimai@gmail.com>
2019-01-21 12:54:08 -0500

    Completed movie-this and concert-this

--- README.md
+++ README.md
@@ -2,37 +2,37 @@
 "Language Interpretation and Recognition Interface" is a command line Node app
 
 *What does this app do?*
-User can input request information for concerts, songs and movies through command-line terminal 
-
-```
-node liri.js *action* *value*
-```
+User can access information for concerts, songs and movies through command-line terminal inputs. There are four different options:
 
 
 1) Search Bands In Town with `concert-this`
 
 ```
-node liri.js concert-this *value*
+node liri.js concert-this <artist/band name>
 ```
+Outputs the name of the venue, venue location and date of the event.
 
 2) Search Spotify with `spotify-this-song`
 
 ```
-node liri.js spotify-this-song *value*
+node liri.js spotify-this-song <song name>
 ```
+Outputs the artist, song name, preview link of the song, and the album that the song is from.
 
-3) Search OMDB with `movie-this`
+3) Search OMDb with `movie-this`
 
 ```
-node liri.js movie-this *value*
+node liri.js movie-this <movie name>
 ```
+Outputs the movide information from the OMDb API, including title, release year, IMDB Rating, Rotten Tomatoes Rating, Country of production, language, plot summary, and actors in the movie.
+
 
 3) Searching contents of text file with `do-what-it-says`
 
 ```
-node liri.js do-what-it-says *<filename.extension>*
+node liri.js do-what-it-says <filename.extension>
 ```
-
+Text file should include the command and search value separated by comma (e.g. )
 
 *How is this app useful?*
 This familiarized the npm package manager through using multiple package modules. Axios npm was used to capture requests and responses to APIs. The dotenv package was used to set a process.env object. Reading text files used the fs package. I learned how to hide API keys by utilizing "dotenv" nmp package and ".gitignore" file that designates which files to not upload to GitHub. I can use this information to go back to the apps created in the first half of the program and hide their API keys.
--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 // Load required packages and files
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
 var fs = require("fs");
 
 //Keys can be retrieved with this:
@@ -29,24 +31,64 @@ switch (action) {
         break;
 }
 
-// Searches the Bands in Town Artist Events API to return concert info
+// Searches the Bands in Town Artist Events API to return concert info on each event
 function concertThis() {
+
+    if (value === undefined) {
+        // if there is no value entered to search for
+        console.log("No Artist or Band name provided");
+    } else {
+        // otherwise process to account for multiple words
+        value = process.argv.splice(3, process.argv.length).join("+");
+    }
+
     var queryURL = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp"
     console.log(queryURL);
 
-    // Name of the venue
-    // Venue location
-    // Date of the Event (use moment to format this as "MM/DD/YYYY")
+    axios.get(queryURL)
+        .then(function (response) {
+            // for loop to console.log out results from multiple events
+            console.log("Got the response back");
 
+            for (var i = 0; i < response.data.length; i++) {
+                // Name of the venue
+                console.log(response.data[i].venue.name);
+                // Venue location: shows "city,state" for US locations, "city, country" for other locations
+                if (response.data[i].venue.region === "") {
+                    console.log(response.data[i].venue.city + ", " + response.data[i].venue.country);
+                } else {
+                    console.log(response.data[i].venue.city + ", " + response.data[i].venue.region);
+                }
+                // Date of the Event
+                var datetime = response.data[i].datetime.split("T")[0]; // takes first index (date only) after splitting by letter 'T'
+                console.log(moment(datetime, 'YYYY-MM-DD').format('MM/DD/YYYY')); // use moment to format date to MM/DD/YYYY
+                console.log("\n"); // adds a new line after each event
+            }
+        })
+        .catch(function (error) {
+            // error message handler used in class exercise
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }
 
-//
+
+// Searches Spotify with song name as input
 function spotifyThisSong() {
     console.log("SpotifyThis");
 
     // Default to "The Sign" by Ace of Base if no song is provided
     if (value === undefined) {
         console.log("No Song provided");
+        value = "The Sign";
     }
 
     // Artist(s)
@@ -58,23 +100,43 @@ function spotifyThisSong() {
 }
 
 function movieThis() {
-    console.log("MovieThis");
 
-    // Default to "Mr. Nobody" if no movie is provided
     if (value === undefined) {
-        console.log("No movie provided");
-        value = "Mr.Nobody"
-    } 
+        // Default to "Mr. Nobody" if no movie is provided
+        console.log("No movie title provided! Displaying default movie information");
+        value = "Mr.Nobody";
+    } else {
+        // after confirming that there is a value, deals with multiple letter titles
+        value = process.argv.splice(3, process.argv.length).join("+");
+    }
 
-    // Title of the movie.
-    // Year the movie came out.
-    // IMDB Rating of the movie.
-    // Rotten Tomatoes Rating of the movie.
-    // Country where the movie was produced.
-    // Language of the movie.
-    // Plot of the movie.
-    // Actors in the movie.
- 
+    var queryURL = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy"
+
+    axios.get(queryURL)
+        .then(function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Year the movie came out: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language of movie: " + response.data.Language);
+            console.log("Plot of the movie: " + response.data.Plot);
+            console.log("Actors in the movie: " + response.data.Actors);
+        })
+        .catch(function (error) {
+            // error message handler used in class exercise
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
 }
 
 function doWhatItSays() {
